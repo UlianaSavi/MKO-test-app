42375cd82cfdc78e244f650f865004bac10c2fb7
Uliana Savitskaua <uljasha.sav@gmail.com>
2023-09-27 00:06:27 +0600

    add base methods for json server and command for start

--- src/app/core/serviсes/api.service.ts
+++ src/app/core/serviсes/api.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  private static URL = 'http://localhost:3000';
+  private static MESSAGES_ROUTE = '/messages';
+
+  getAll = async () => {
+    const res = await fetch(`${ApiService.URL}${ApiService.MESSAGES_ROUTE}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return res;
+  }
+
+  getById = async (id: string) => {
+    const res = await fetch(`${ApiService.URL}${ApiService.MESSAGES_ROUTE}/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    return res;
+  }
+
+  create = async (id: string, dataToCreate: unknown) => {
+    const res = await fetch(`${ApiService.URL}/${id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ dataToCreate }),
+    });
+
+    return res;
+  }
+
+  update = async (id: string, dataToUpdate: unknown) => {
+    const res = await fetch(`${ApiService.URL}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ dataToUpdate }),
+    });
+
+    return res;
+  }
+
+  remove = async (id: string) => {
+    const res = await fetch(`${ApiService.URL}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
--- src/app/table/components/table-body/table-body.component.ts
+++ src/app/table/components/table-body/table-body.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./table-body.component.scss']
 })
 export class TableBodyComponent {
-
 }
